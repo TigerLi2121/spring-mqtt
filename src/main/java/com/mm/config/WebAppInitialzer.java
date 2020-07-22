178a855c91a1f4982175cd6f5881c56300bf99f0
liwulin <83432087@qq.com>
2020-07-22 21:50:29 +0800

    update

--- src/main/java/com/mm/config/WebAppInitialzer.java
+++ src/main/java/com/mm/config/WebAppInitialzer.java
@@ -1,6 +1,10 @@
 package com.mm.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class WebAppInitialzer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,12 @@ public class WebAppInitialzer extends AbstractAnnotationConfigDispatcherServletI
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.addFilter("name", new CharacterEncodingFilter("UTF-8", true))
+                .addMappingForUrlPatterns(null, false, "/*");
     }
 }
--- src/main/java/com/mm/controller/IndexController.java
+++ src/main/java/com/mm/controller/IndexController.java
@@ -2,19 +2,33 @@ package com.mm.controller;
 
 import com.mm.mqtt.MqttSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class IndexController {
 
     @Autowired
     private MqttSender mqttSender;
 
     @GetMapping("/send/{topic}")
-    public String hello(@PathVariable("topic") String topic, String msg) {
+    @ResponseBody
+    public String send(@PathVariable("topic") String topic, String msg) {
         mqttSender.sendToMqtt(topic, msg);
         return msg;
+    }
+
+    @GetMapping("/hello")
+    public String hello(String msg, Model model) {
+        model.addAttribute("msg", msg);
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
